7a8cbf369b12ddf53b3e8063c89d1d63fb42279b
vantreeseba <vantreeseba@gmail.com>
2014-01-10 01:58:48 -0600

    adding connection string stuff to db.bootstrap

--- bootstrap/db-bootstrap.sh
+++ bootstrap/db-bootstrap.sh
@@ -2,3 +2,7 @@
 
 sudo apt-get update
 sudo apt-get install -y postgresql
+
+sudo -u postgres psql -c "CREATE USER web_worker WITH PASSWORD 'pass';
+						  CREATE DATABASE web_worker owner web_worker;"
+sudo -u postgres psql -d web_worker -c "CREATE TABLE messages (message VARCHAR(255));"
